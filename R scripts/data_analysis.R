cb2cd43c2127e25764779bda58cf0d8d5e67c6b7
Kauane <kauanembordin@gmail.com>
2022-01-19 20:13:10 +0000

    minor corrections

--- R scripts/data_analysis.R
+++ R scripts/data_analysis.R
@@ -1,37 +1,56 @@
 # Load packages -----
-library(nlme);library(vegan);library(here);library(MuMIn)
-library(ggplot2)
+library(nlme) #linear mixed models
+library(vegan) #data standardization
+library(here) #load data
+library(MuMIn) #model selection
 
 
 # Load data -----
-data <- read.table(here::here("Data", "data.txt"), header=T, row.names = 1) #load data
+#load data
+data <- read.table(here::here("Data", "data.txt"), header=T, row.names = 1) 
 head(data)
 
-std <- decostand(data[,7:17], method = "standardize",na.rm = T) #data standardization
+#data standardization
+std <- decostand(data[,7:17], method = "standardize",na.rm = T) 
 head(std)
 
-variables <- cbind(data[,1:6],std) #combining data
+#new df containing standardized data
+variables <- cbind(data[,1:6],std) 
 head(variables)
+
+#convert Forest Age column as a factor
 variables$ForestAge <- as.factor(variables$ForestAge)
 
 # Data analysis -----
-# Biodiversity metrics
+# Biodiversity metrics: taxonomic, functional and phylogenetic diversity
+
+#null model
 net  <- lme(NetCarbonChange~1, random=~1|ForestAge,correlation=corExp(form=~Longitude+Latitude),weights = ~Weight, data=variables) 
+#complete model with ForestAge as an interaction factor
 net0 <- lme(NetCarbonChange~(TaxDiv+FD+PD)*ForestAge, random=~1|CensusInterval,correlation=corExp(form=~Longitude+Latitude),weights = ~Weight, data=variables) 
+#Complete model
 net1 <- lme(NetCarbonChange~TaxDiv+FD+PD, random=~1|ForestAge,correlation=corExp(form=~Longitude+Latitude),weights = ~Weight, data=variables) 
+#Only taxonomic diversity
 net2 <- lme(NetCarbonChange~TaxDiv, random=~1|ForestAge,correlation=corExp(form=~Longitude+Latitude),weights = ~Weight, data=variables) 
+#Only Functional diversity
 net3 <- lme(NetCarbonChange~FD, random=~1|ForestAge,correlation=corExp(form=~Longitude+Latitude),weights = ~Weight, data=variables) 
+#Only phylogenetic diversity
 net4 <- lme(NetCarbonChange~PD, random=~1|ForestAge,correlation=corExp(form=~Longitude+Latitude),weights = ~Weight, data=variables) 
 
+#model selection
 AIC(net, net0, net1, net2, net3, net4)
-
+#best model results
 summary(net3)
 r.squaredGLMM(net3)
 
-# Functional trait diversity
+# Functional trait diversity --
+#predictive power of trait diversity on net carbon change
 net_fd <- lme(NetCarbonChange ~ FD.WD+FD.SLA+FD.LDMC+FD.Hmax, random=~1|ForestAge, 
               correlation=corExp(form=~Longitude+Latitude),weight=~Weight, data=variables) 
 options(na.action = "na.fail")
+#model selection
 model.sel<-dredge(net_fd)
+#summarise model selection results
 net_fd.avg<-summary(model.avg(model.sel, subset = delta < 4)) ; net_fd.avg
+#only best model
 model.sel
