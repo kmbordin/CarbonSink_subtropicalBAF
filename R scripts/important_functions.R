63c3db2fb697143cda9a25b57f4b8311304631b7
Kauane <kauanembordin@gmail.com>
2022-01-19 20:13:36 +0000

    script with ms important functions

---
+++ R scripts/important_functions.R
@@ -0,0 +1,45 @@
+#You may find some important functions below
+#See also Supplementary Information 2
+
+#unction to convert factor to numeric
+f2n <- function(f) {  
+  if(!is.factor(f)) 
+  stop("the input must be a factor") 
+  as.numeric(levels(f))[as.integer(f)]
+  }
+
+#function to calculate basal area (m2/ha)
+basal.area<- function(x) { 
+  (pi*(x/2)^2)/10000
+} 
+
+#Equation to obtain above-ground woody productivity (AGWP), from Kohyama et al. 2019
+Pann<-function(Bs0,B0,Bt,t) {
+  ((1-((Bs0/Bt)^(1/t)))*(Bt-B0))/((t*(1-(B0/Bt)^(1/t))))
+  }
+
+#Equation to obtain carbon lçosses through mortality, from Kohyama et al. 2019
+Lann<- function (Bs0,B0,Bt,t){
+  ((1-((Bs0/B0)^(1/t)))*(Bt-B0))/((t*(((Bt/B0)^(1/t))-1)))
+}
+
+
+#Above-ground biomass estimation: 
+require(BiomasaFP)
+BiomasaFP::AGBChv14 
+
+#Taxonomic diversity: 
+require(vegan)
+vegan:: diversity(index = "inv")
+
+#Functional diversity:
+require(FD)
+FD:: dbFD 
+
+#Phylogenetic tree construction: 
+require(V.PhyloMaker)
+V.PhyloMaker::phylo.maker
+
+#Phylogenetic diversity
+require(picante) 
+picante:: mpd
