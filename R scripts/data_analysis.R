617ca0533e9530281d114a07a35bd12b9086a8d0
Kauane <kauanembordin@gmail.com>
2022-08-15 15:33:40 -0300

    updates after R1

--- R scripts/data_analysis.R
+++ R scripts/data_analysis.R
@@ -2,7 +2,6 @@
 library(nlme) #linear mixed models
 library(vegan) #data standardization
 library(here) #load data
-library(MuMIn) #model selection
 
 
 # Load data -----
@@ -11,7 +10,7 @@ data <- read.table(here::here("Data", "data.txt"), header=T, row.names = 1)
 head(data)
 
 #data standardization
-std <- decostand(data[,7:17], method = "standardize",na.rm = T) 
+std <- decostand(data[,7:13], method = "standardize",na.rm = T) 
 head(std)
 
 #new dataframe containing standardized data
@@ -27,7 +26,7 @@ variables$ForestAge <- as.factor(variables$ForestAge)
 #null model
 net  <- lme(NetCarbonChange~1, random=~1|ForestAge,correlation=corExp(form=~Longitude+Latitude),weights = ~Weight, data=variables) 
 #complete model with ForestAge as an interaction factor
-net0 <- lme(NetCarbonChange~(TaxDiv+FD+PD)*ForestAge, random=~1|CensusInterval,correlation=corExp(form=~Longitude+Latitude),weights = ~Weight, data=variables) 
+net0 <- gls(NetCarbonChange~(TaxDiv+FD+PD)*ForestAge, correlation=corExp(form=~Longitude+Latitude),weights = ~Weight, data=variables) 
 #Complete model
 net1 <- lme(NetCarbonChange~TaxDiv+FD+PD, random=~1|ForestAge,correlation=corExp(form=~Longitude+Latitude),weights = ~Weight, data=variables) 
 #Only taxonomic diversity
@@ -39,18 +38,5 @@ net4 <- lme(NetCarbonChange~PD, random=~1|ForestAge,correlation=corExp(form=~Lon
 
 #model selection
 AIC(net, net0, net1, net2, net3, net4)
-#best model results
-summary(net3)
-r.squaredGLMM(net3)
 
-# Functional trait diversity --
-#predictive power of trait diversity on net carbon change
-net_fd <- lme(NetCarbonChange ~ FD.WD+FD.SLA+FD.LDMC+FD.Hmax, random=~1|ForestAge, 
-              correlation=corExp(form=~Longitude+Latitude),weight=~Weight, data=variables) 
-options(na.action = "na.fail")
-#model selection
-model.sel<-dredge(net_fd)
-#summarise model selection results
-net_fd.avg<-summary(model.avg(model.sel, subset = delta < 4)) ; net_fd.avg
-#only best model
-model.sel
+
